d6551fb4d723098243c6ffec4e7f0ba89ac735ae
27JuneWMF2Rutvik <bhambhirutvik2004@gmail.com>
2023-03-23 18:13:30 +0530

    update

---
+++ modules/main.js
@@ -0,0 +1,19 @@
+
+import { user } from "./bridge";
+
+user();
+
+
+// import Rutvik from "./library";
+// import {message, user} from "./library";
+
+// Rutvik();
+
+
+// import * as Rutvik from "./library";
+
+// console.log(message);
+
+// console.log(us("Rutvik"));
+
+// let a = new Rutvik.test();
