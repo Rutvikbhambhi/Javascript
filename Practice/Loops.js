3582b02bac515221bb703a62bc85a7a94d1d4833
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-05-31 10:01:00 +0530

    js

--- Practice/Loops.js
+++ Practice/Loops.js
@@ -1,8 +1,25 @@
 
 // while loop
-let i = 0;
+// let i = 0;
 
-while(i<=3){
-    console.log(i);
-    i++;
-}
+// while(i<=3){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Example 
+let num = 10;
+// let total = 0;
+// let i = 0;
+
+// while(i<=10){
+//     total = total + i;
+//     i++;
+// }
+
+// console.log(total);
+
+let total = (num*(num+1))/2;
+console.log(total);
+
