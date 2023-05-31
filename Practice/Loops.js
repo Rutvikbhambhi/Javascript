b1ffe254791d5062073ff6025fbc858915fd4966
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-05-31 15:10:14 +0530

    js

--- Practice/Loops.js
+++ Practice/Loops.js
@@ -9,7 +9,7 @@
 
 
 // Example 
-let num = 10;
+// let num = 10;
 // let total = 0;
 // let i = 0;
 
@@ -20,6 +20,53 @@ let num = 10;
 
 // console.log(total);
 
-let total = (num*(num+1))/2;
-console.log(total);
+// let total = (num*(num+1))/2;
+// console.log(total);
 
+
+
+// For loop
+// for(let i = 0; i<=9; i++) {
+//     console.log(i);
+// }
+
+// Example
+// let total = 0;
+// let num = 10;
+
+// for(let i = 1; i<=num; i++) {
+//     total = total + i;
+// }
+
+// console.log(total);
+
+
+
+// break keyword
+
+// continue keyword
+
+// for(let i = 1; i<=10; i++) {
+//     if(i===4){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 4) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+// do while loop
+let i = 10;
+do {
+    console.log(i);
+    i++;
+}while(i<=9);
+
+console.log("value of i is", i);
