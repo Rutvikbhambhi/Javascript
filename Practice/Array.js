2c7b437e52369cb6e73dacc1c26ccb3b169e19a8
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-06-01 15:50:05 +0530

    js

--- Practice/Array.js
+++ Practice/Array.js
@@ -26,9 +26,9 @@
 // console.log(fruits);
 
 // unshift 
-let fruits = [1,2,3,4,5];
-fruits.unshift(0)
-console.log(fruits);
+// let fruits = [1,2,3,4,5];
+// fruits.unshift(0)
+// console.log(fruits);
 
 
 
@@ -43,12 +43,89 @@ console.log(fruits);
 // console.log("value is num2 is", num2);
 
 
+
 // reference types 
+// let array1 = ["item1", "item2"];
+// let array2 = array1;
+// console.log("array1", array1);
+// console.log("array2", array2);
+// array1.push("item3");
+// console.log("after pushing element to array 1");
+// console.log("array1", array1);
+// console.log("array2", array2);
+
+
+
+// how to clone array
+
+// how to concatenate two array
+
 let array1 = ["item1", "item2"];
-let array2 = array1;
-console.log("array1", array1);
-console.log("array2", array2);
+// let array2 = ["item1", "item2"];
+// let array2 = array1.slice(0);
+let array2 = [].concat(array1, ["item3", "item4"]);
+
+
+// spread operator
+// let array2 = [...array1];
 array1.push("item3");
-console.log("after pushing element to array 1");
-console.log("array1", array1);
-console.log("array2", array2);
+
+console.log(array1===array2);
+console.log(array1);
+console.log(array2);
+
+
+// For loop in Array 
+// let fruits = ["apple", "mango", "grapes", "banana"];
+
+// for(let i=0; i<=9; i++) {
+//     console.log(i);
+// }
+
+// for(let i=0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+// console.log(fruits[fruits.length-2]);
+
+
+// use const for creating array
+// const fruits = ["apple", "mango"];
+// fruits.push("banana");
+// console.log(fruits);
+
+
+// while loop in array 
+// const fruits = ["apple", "mango", "grapes"];
+// const fruits2 = [];
+
+// let i = 0;
+// while(i<fruits.length){
+//     fruits2.push(fruits[i].toUpperCase());
+//     i++;
+// }
+// console.log(fruits2);
+
+
+// For in loop in array 
+const fruits = ["apple", "mango", "grapes", "papita"] 
+const fruits2 = [];
+
+// for(let index in fruits) {
+//     fruits2.push(fruits[index].toLowerCase())
+// }
+// console.log(fruits2);
+
+for(let i = 0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
+
+
+// array destructuring
+const myArray = ["value", "value2", "value3", "value4"];
+// let myvar1 = myArray[0];
+// let myvar2 = myArray[1];
+// console.log("value of myvar1", myvar1);
+let [myvar1, myvar2, ...myNewArray] = myArray;
+console.log("value of myvar1", myvar1);
+console.log("value of myvar2", myvar2);
+console.log(myNewArray);
