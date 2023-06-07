fc5382ffc874049de686a7fef9f333cf8ebc15a9
Rutvikbhambhi <bhambhirutvik2004@gmail.com>
2023-06-07 12:57:17 +0530

    js

--- Practice/Arrow-Function.js
+++ Practice/Arrow-Function.js
@@ -1,4 +1,22 @@
+// arrow functions 
+const users = () => {
+    console.log("user are already login");
+}
+users();
+
 
 const isEven = function(number){
     return number % 2 === 0;
+}
+
+// const isEven = number => number % 2 === 0;
+console.log(isEven(4));
+
+const findTarget = (array, target) => {
+    for(let i = 0; i<array.length; i++) {
+        if(array[i]===target){
+            return i;
+        }
+    }
+    return -1;
 }
--- Practice/Function.js
+++ Practice/Function.js
@@ -29,21 +29,52 @@ users();
 // console.log(isEven(2));
 
 
-function findTarget(array, target) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i]===target) {
-            return i;
-        }
+// const findTarget = function(array, target) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i]===target) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// const myArray = [1,3,8,90];
+// const ans = findTarget(myArray, 8);
+// console.log(ans);
+
+
+// const num = [1,2,3,4]
+//     num.slice(2)
+//     num.slice(1,3)
+
+// console.log(num);
+
+
+// function inside function
+const app = () => {
+    const myFunc = () => {
+        console.log("hello from myFunc");
     }
-    return -1;
+
+    const addTwo = (num1, num2) => {
+        return num1 + num2;
+    }
+
+    const mul = (num1, num2) => num1 * num2; 
+    console.log("inside app");
+    myFunc();
+    console.log(addTwo(2,3));
+    console.log(mul(2,3));
 }
-const myArray = [1,3,8,90];
-const ans = findTarget(myArray, 8);
-console.log(ans);
+app();
 
 
-const num = [1,2,3,4]
-    num.slice(2)
-    num.slice(1,3)
 
-console.log(num);
+// lexical scope 
+function myApp(){
+    const myVar = "value1";
+    function myFunc(){
+        // const my
+    }
+    const myFunc2 = function() {}
+    const myFunc3 = () => {}
+}
